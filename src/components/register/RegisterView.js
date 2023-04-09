fccd73c682100ffd7f5220dd88a843ab52e5fb16
DinnoCreator <edijay17@gmail.com>
2023-04-09 11:48:01 +0100

    modified:   src/components/register/RegisterView.js

--- src/components/register/RegisterView.js
+++ src/components/register/RegisterView.js
@@ -3,6 +3,7 @@ import { useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { logo } from "../../images/images";
 import { api } from "../../link/API";
+import { motion } from "framer-motion";
 
 const RegisterView = () => {
   // values inputed in the form
@@ -51,8 +52,8 @@ const RegisterView = () => {
         if (res.status === 401) {
           setDip("block");
           setLoading(false);
-          return setLoginError("Email already in use...");}
-        else if (res.status === 411) {
+          return setLoginError("Email already in use...");
+        } else if (res.status === 411) {
           setDip("block");
           setLoading(false);
           return setLoginError("Something went wrong...");
@@ -101,136 +102,143 @@ const RegisterView = () => {
             </h5>
           </div>
         </div>
-        <div className={` mt-1 mb-4 ${classes.bod} form-signin`}>
-          <img
-            className={`${classes.bodimg} block centerMarg`}
-            src={logo}
-            alt="smartsheLogo"
-            width="120px"
-          />
-          {/* <h3>SmartSHE</h3> */}
-          <div className="container">
-            {loginError && ( // then if changed flag is false show error message.
-              <div
-                className="container"
-                style={{ color: "red", display: { dip } }}
-              >
-                <span>{loginError}</span>
-              </div>
-            )}
-            {/* <form className="container"> */}
-            <form className="container" onSubmit={onSubmitForm}>
-              <div className="mb-1">
-                <label htmlFor="exampleInputEmail1" className="form-label">
-                  <span className="red">*</span> Company's email
-                </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleInputEmail1"
-                  autoComplete="off"
-                  aria-describedby="emailHelp"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-1">
-                <label htmlFor="mail1" className="form-label">
-                  <span className="red">*</span> Company's name
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="mail1"
-                  autoComplete="off"
-                  aria-describedby="mailHelp"
-                  value={userName}
-                  onChange={(e) => setUserName(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-1">
-                <label htmlFor="RegNo" className="form-label">
-                  <span className="red">*</span> Company's CAC RegNo
-                </label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="RegNo"
-                  autoComplete="off"
-                  minLength="8"
-                  maxLength="8"
-                  pattern="[A-Za-z0-9]+"
-                  value={userReg}
-                  onChange={(e) => setUserReg(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-1">
-                <label htmlFor="phone" className="form-label">
-                  <span className="red">*</span> Company's phone no
-                </label>
-                <input
-                  type="tell"
-                  className="form-control"
-                  id="phone"
-                  autoComplete="off"
-                  minLength="11"
-                  maxLength="11"
-                  pattern="[0-9]+"
-                  value={userPhone}
-                  onChange={(e) => setUserPhone(e.target.value)}
-                  required
-                />
-              </div>
-              <div className="mb-1">
-                <label htmlFor="exampleInputPassword1" className="form-label">
-                  <span className="red">*</span> Password
-                </label>
-                <span className={`d-flex ${classes.white}`}>
+
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className={` mt-1 mb-4 ${classes.bod} form-signin`}>
+            <img
+              className={`${classes.bodimg} block centerMarg`}
+              src={logo}
+              alt="smartsheLogo"
+              width="120px"
+            />
+            {/* <h3>SmartSHE</h3> */}
+            <div className="container">
+              {loginError && ( // then if changed flag is false show error message.
+                <div
+                  className="container"
+                  style={{ color: "red", display: { dip } }}
+                >
+                  <span>{loginError}</span>
+                </div>
+              )}
+              {/* <form className="container"> */}
+              <form className="container" onSubmit={onSubmitForm}>
+                <div className="mb-1">
+                  <label htmlFor="exampleInputEmail1" className="form-label">
+                    <span className="red">*</span> Company's email
+                  </label>
                   <input
-                    type="password"
-                    className="form-control me-2"
-                    id="exampleInputPassword1"
+                    type="email"
+                    className="form-control"
+                    id="exampleInputEmail1"
                     autoComplete="off"
-                    ref={pass}
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    aria-describedby="emailHelp"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     required
                   />
-                  <button
-                    className={`btn ${classes.eye}`}
-                    onClick={showPassword}
-                    type="button"
-                  >
-                    <i className={`fa-regular ${eye}`}></i>
-                  </button>
-                </span>
-                <div id="emailHelp" className="form-text">
-                  min. 8 characters
                 </div>
-              </div>
-              <div className="d-grid gap-2 ">
-                <button
-                  className={`btn bold bg-brown bottomShadow btnct btnct-white ${classes.login}`}
-                  type="submit"
-                >
-                  {loading ? (
-                    <>
-                      <div
-                        style={{ display: "inline-block" }}
-                        className="load"
-                      ></div>
-                    </>
-                  ) : (
-                    <>Signup</>
-                  )}
-                </button>
-              </div>
-            </form>
+                <div className="mb-1">
+                  <label htmlFor="mail1" className="form-label">
+                    <span className="red">*</span> Company's name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="mail1"
+                    autoComplete="off"
+                    aria-describedby="mailHelp"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-1">
+                  <label htmlFor="RegNo" className="form-label">
+                    <span className="red">*</span> Company's CAC RegNo
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="RegNo"
+                    autoComplete="off"
+                    minLength="8"
+                    maxLength="8"
+                    pattern="[A-Za-z0-9]+"
+                    value={userReg}
+                    onChange={(e) => setUserReg(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-1">
+                  <label htmlFor="phone" className="form-label">
+                    <span className="red">*</span> Company's phone no
+                  </label>
+                  <input
+                    type="tell"
+                    className="form-control"
+                    id="phone"
+                    autoComplete="off"
+                    minLength="11"
+                    maxLength="11"
+                    pattern="[0-9]+"
+                    value={userPhone}
+                    onChange={(e) => setUserPhone(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-1">
+                  <label htmlFor="exampleInputPassword1" className="form-label">
+                    <span className="red">*</span> Password
+                  </label>
+                  <span className={`d-flex ${classes.white}`}>
+                    <input
+                      type="password"
+                      className="form-control me-2"
+                      id="exampleInputPassword1"
+                      autoComplete="off"
+                      ref={pass}
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                    <button
+                      className={`btn ${classes.eye}`}
+                      onClick={showPassword}
+                      type="button"
+                    >
+                      <i className={`fa-regular ${eye}`}></i>
+                    </button>
+                  </span>
+                  <div id="emailHelp" className="form-text">
+                    min. 8 characters
+                  </div>
+                </div>
+                <div className="d-grid gap-2 ">
+                  <button
+                    className={`btn bold bg-brown bottomShadow btnct btnct-white ${classes.login}`}
+                    type="submit"
+                  >
+                    {loading ? (
+                      <>
+                        <div
+                          style={{ display: "inline-block" }}
+                          className="load"
+                        ></div>
+                      </>
+                    ) : (
+                      <>Signup</>
+                    )}
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
+        </motion.div>
       </>
     );
   }
